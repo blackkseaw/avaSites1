c7ab1a64c468efec264f180eb4377d8851ac4b62
blackkseaw <blackkseaw@gmail.com>
2023-06-01 21:23:42 -0300

    Primeiro commit da prova

---
+++ script.js
@@ -0,0 +1,9 @@
+function validarSenha(){
+    password = document.getElementById('password').value;
+    if (password.length < 8){
+       alert("SENHA MUITO PEQUENA"); 
+    }else{
+        alert("SENHA BOA IGUAL CAFE"); 
+    }
+ }
+   
